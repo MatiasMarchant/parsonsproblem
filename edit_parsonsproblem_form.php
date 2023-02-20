fa764f8f40d0c29b35f8ad2f4df45368cad50093
Matías <matias.marchant@sansano.usm.cl>
2023-02-20 17:14:58 -0300

    Initial commit, after using Moodle plugin skeleton generator

---
+++ edit_parsonsproblem_form.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * The editing form for parsonsproblem question type is defined here.
+ *
+ * @package     qtype_parsonsproblem
+ * @copyright   2023 Matías Marchant <matias.marchant@sansano.usm.cl>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * parsonsproblem question editing form defition.
+ *
+ * You should override functions as necessary from the parent class located at
+ * /question/type/edit_question_form.php.
+ */
+class qtype_parsonsproblem_edit_form extends question_edit_form {
+
+    /**
+     * Returns the question type name.
+     *
+     * @return string The question type name.
+     */
+    public function qtype() {
+        return 'parsonsproblem';
+    }
+}
