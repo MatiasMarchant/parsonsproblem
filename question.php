f56de7c316f30c01e08f0a5c11597b9a82bea1f5
Matías <matias.marchant@sansano.usm.cl>
2023-02-21 14:47:28 -0300

    Abstract functions of question_graded_automatically parent class get defined to avoid errors

--- question.php
+++ question.php
@@ -53,6 +53,78 @@ class qtype_parsonsproblem_question extends question_graded_automatically {
     }
 
     /**
+     * Used by many of the behaviours, to work out whether the student's
+     * response to the question is complete. That is, whether the question attempt
+     * should move to the COMPLETE or INCOMPLETE state.
+     *
+     * @param array $response responses, as returned by
+     *      {@link question_attempt_step::get_qt_data()}.
+     * @return bool whether this response is a complete answer to this question.
+     */
+    public function is_complete_response(array $response)
+    {
+        return true;
+    }
+
+    /**
+     * Use by many of the behaviours to determine whether the student's
+     * response has changed. This is normally used to determine that a new set
+     * of responses can safely be discarded.
+     *
+     * @param array $old the responses previously recorded for this question,
+     *      as returned by {@link question_attempt_step::get_qt_data()}
+     * @param array $new the new responses, in the same format.
+     * @return bool whether the two sets of responses are the same - that is
+     *      whether the new set of responses can safely be discarded.
+     */
+    public function is_same_response(array $prevresponse, array $newresponse)
+    {
+        return null;
+    }
+
+    /**
+     * Produce a plain text summary of a response.
+     *
+     * @param array $response a response, as might be passed to {@link grade_response()}.
+     * @return string a plain text summary of that response, that could be used in reports.
+     */
+    public function summarise_response(array $response)
+    {
+        // if (isset($response[$this->get_response_fieldname()])) {
+        //     return $response[$this->get_response_fieldname()];
+        // } else {
+        //     return null;
+        // }
+        return "";
+    }
+
+    /**
+     * In situations where is_gradable_response() returns false, this method
+     * should generate a description of what the problem is.
+     * @param array $response
+     * @return string the message
+     */
+    public function get_validation_error(array $response)
+    {
+        return null;
+    }
+
+    /**
+     * Grade a response to the question, returning a fraction between
+     * get_min_fraction() and get_max_fraction(), and the corresponding {@link question_state}
+     * right, partial or wrong.
+     *
+     * @param array $response responses, as returned by
+     *      {@link question_attempt_step::get_qt_data()}.
+     * @return array (float, integer) the fraction, and the state.
+     */
+    public function grade_response(array $response)
+    {
+        $grade = array(1, question_state::graded_state_for_fraction(1));
+        return $grade;
+    }
+
+    /**
      * Checks whether the user is allowed to be served a particular file.
      *
      * @param question_attempt $qa The question attempt being displayed.
